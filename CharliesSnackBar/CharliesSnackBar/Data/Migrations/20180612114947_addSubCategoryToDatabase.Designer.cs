8651d3e5f77fe496e485f0c4cf829daa8838ba4e
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-12 23:56:52 +1200

    Added SubCategory Model to Database

--- CharliesSnackBar/CharliesSnackBar/Data/Migrations/20180612114947_addSubCategoryToDatabase.Designer.cs
+++ CharliesSnackBar/CharliesSnackBar/Data/Migrations/20180612114947_addSubCategoryToDatabase.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CharliesSnackBar.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180612114947_addSubCategoryToDatabase")]
+    partial class addSubCategoryToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
