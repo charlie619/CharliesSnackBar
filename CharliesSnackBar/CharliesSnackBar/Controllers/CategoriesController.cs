129085f1a570682531fb5e805945e4d343a51b6c
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-12 17:06:06 +1200

    Updated View of Edit Action.

--- CharliesSnackBar/CharliesSnackBar/Controllers/CategoriesController.cs
+++ CharliesSnackBar/CharliesSnackBar/Controllers/CategoriesController.cs
@@ -86,7 +86,7 @@ namespace CharliesSnackBar.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,DsplayOrder")] Category category)
+        public async Task<IActionResult> Edit(int id, Category category)
         {
             if (id != category.Id)
             {
