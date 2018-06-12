119f53551835f5c761dd7f5931f0413168f2d522
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-13 00:24:50 +1200

    Added SubCategories Controller & Index action Method.

---
+++ CharliesSnackBar/CharliesSnackBar/Controllers/SubCategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CharliesSnackBar.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CharliesSnackBar.Controllers
+{
+    public class SubCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SubCategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //Get Action
+        public async Task<IActionResult> Index()
+        {
+            var subCategories = _db.SubCategory.Include(x => x.Category);
+            return View(await subCategories.ToListAsync());
+        }
+    }
+}
