ee65a332852c79e58b468f1a95ff96c0ea7b405c
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-20 01:42:35 +1200

    Added Coupons Controller and ndex Action Method

---
+++ CharliesSnackBar/CharliesSnackBar/Controllers/CouponsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CharliesSnackBar.Data;
+using CharliesSnackBar.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CharliesSnackBar.Controllers
+{
+    public class CouponsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Coupons.ToListAsync());
+        }
+    }
+}
