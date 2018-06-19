dea7b6727ca9f7299b7e4cc1bafa1d08f891e1b6
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-19 14:19:20 +1200

    Added Create Post Action to save Items with Images.

--- CharliesSnackBar/CharliesSnackBar/Controllers/MenuItemsController.cs
+++ CharliesSnackBar/CharliesSnackBar/Controllers/MenuItemsController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CharliesSnackBar.Data;
 using CharliesSnackBar.Models;
 using CharliesSnackBar.Models.MenuItemViewModels;
+using CharliesSnackBar.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -13,9 +15,9 @@ using Microsoft.EntityFrameworkCore;
 namespace CharliesSnackBar.Controllers
 {
     public class MenuItemsController : Controller
-    {        
-            private readonly ApplicationDbContext _db;
-            private readonly IHostingEnvironment _hostingEnvironment;
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
         [BindProperty]
         public MenuItemViewModel MenuItemVM { get; set; }
@@ -30,7 +32,7 @@ namespace CharliesSnackBar.Controllers
                 MenuItem = new Models.MenuItem()
             };
         }
-    
+
         //Get : MenuItems
         public async Task<IActionResult> Index()
         {
@@ -44,6 +46,49 @@ namespace CharliesSnackBar.Controllers
             return View(MenuItemVM);
         }
 
+        //POST : MenuItems Create
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePOST()
+        {
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+            if (!ModelState.IsValid)
+            {
+                return View(MenuItemVM);
+            }
+
+            _db.MenuItem.Add(MenuItemVM.MenuItem);
+            await _db.SaveChangesAsync();
+
+            //Image Being Saved
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var menuItemFromDb = _db.MenuItem.Find(MenuItemVM.MenuItem.Id);
+
+            if (files[0] != null && files[0].Length > 0)
+            {
+                var uploads = Path.Combine(webRootPath, "images");
+                var extension = files[0].FileName.Substring(files[0].FileName.LastIndexOf("."), files[0].FileName.Length - files[0].FileName.LastIndexOf("."));
+
+                using (var filestream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filestream);
+                }
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension;
+            }        
+            else
+	        {
+                //When user does not upload image
+                var uploads = Path.Combine(webRootPath, @"images\" + SD.DefaultFoodmage);
+                System.IO.File.Copy(uploads, webRootPath + @"\images\" + MenuItemVM.MenuItem.Id + ".png");
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + ".png";
+            }
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
         public JsonResult GetSubCategory(int CategoryId)
         {
             var subCategoryList = new List<SubCategory>();
---
+++ CharliesSnackBar/CharliesSnackBar/Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharliesSnackBar.Utility
+{
+    public class SD
+    {
+        public const string DefaultFoodmage = "default_food.png";
+    }
+}
