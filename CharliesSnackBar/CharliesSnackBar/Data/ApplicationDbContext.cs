c4dd24a56154df1bee013ae5fdd0166e293f0cc5
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-20 01:36:14 +1200

    Add Coupons Model to DB

--- CharliesSnackBar/CharliesSnackBar/Data/ApplicationDbContext.cs
+++ CharliesSnackBar/CharliesSnackBar/Data/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace CharliesSnackBar.Data
         public DbSet<Category> Category { get; set; }
         public DbSet<SubCategory> SubCategory { get; set; }
         public DbSet<MenuItem> MenuItem { get; set; }
+        public DbSet<Coupons> Coupons { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
--- CharliesSnackBar/CharliesSnackBar/Data/Migrations/20180619133502_AddCouponsToDb.Designer.cs
+++ CharliesSnackBar/CharliesSnackBar/Data/Migrations/20180619133502_AddCouponsToDb.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CharliesSnackBar.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180619133502_AddCouponsToDb")]
+    partial class AddCouponsToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
