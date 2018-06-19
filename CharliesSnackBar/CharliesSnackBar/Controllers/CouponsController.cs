764405a9348a1de184d2a593790826687437ff6e
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-20 02:09:13 +1200

    Added Create GET and POST Method for Coupons

--- CharliesSnackBar/CharliesSnackBar/Controllers/CouponsController.cs
+++ CharliesSnackBar/CharliesSnackBar/Controllers/CouponsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CharliesSnackBar.Data;
@@ -21,6 +22,40 @@ namespace CharliesSnackBar.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _db.Coupons.ToListAsync());
+        }
+
+        //GET : Coupons
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST : Create Coupons
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupons coupons)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files[0]!=null && files[0].Length>0)
+                {
+                    byte[] p1 = null;
+                    using(var fs1 = files[0].OpenReadStream())
+                    {
+                        using(var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }                       
+                    }
+                    coupons.Picture = p1;
+                    _db.Coupons.Add(coupons);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(coupons);
         }
     }
 }
