16c11d6532a747ff1de62c17e1719da48f4a7509
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-20 20:11:39 +1200

    Added FirstName and LastName property to ASPNetUsers Table

--- CharliesSnackBar/CharliesSnackBar/Data/Migrations/20180620080522_AddFirstNameAndLastNameToUsers.Designer.cs
+++ CharliesSnackBar/CharliesSnackBar/Data/Migrations/20180620080522_AddFirstNameAndLastNameToUsers.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CharliesSnackBar.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180620080522_AddFirstNameAndLastNameToUsers")]
+    partial class AddFirstNameAndLastNameToUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
