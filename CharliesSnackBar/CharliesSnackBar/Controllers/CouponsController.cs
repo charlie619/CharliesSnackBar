d0dff08f26da6c4c79ce5cf772aed69b16cb9ea9
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-20 13:21:23 +1200

    Added Edit GET & POST Action Method

--- CharliesSnackBar/CharliesSnackBar/Controllers/CouponsController.cs
+++ CharliesSnackBar/CharliesSnackBar/Controllers/CouponsController.cs
@@ -57,5 +57,61 @@ namespace CharliesSnackBar.Controllers
             }
             return View(coupons);
         }
+
+        //GET : Edit Coupons
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupons.SingleOrDefaultAsync(x => x.Id == id);
+
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+        }
+
+        //POST : Edit Coupons
+        public async Task<IActionResult> Edit(int id, Coupons coupons)
+        {
+            if (id!=coupons.Id)
+            {
+                return NotFound();
+            }
+
+            var couponFromDb = await _db.Coupons.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files[0] != null && files[0].Length > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    coupons.Picture = p1;
+                }
+                couponFromDb.MinimumAmount = coupons.MinimumAmount;
+                couponFromDb.Name = coupons.Name;
+                couponFromDb.Discount = coupons.Discount;
+                couponFromDb.CouponType = coupons.CouponType;
+                couponFromDb.IsActive = coupons.IsActive;
+
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupons);
+        }
     }
 }
