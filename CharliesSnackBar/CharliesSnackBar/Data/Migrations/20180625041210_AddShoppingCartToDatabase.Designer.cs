c6603664976072defddb2291c90b9f1a2c3f1d9d
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-25 16:14:25 +1200

    Added Shopping Cart Model to DB

--- CharliesSnackBar/CharliesSnackBar/Data/Migrations/20180625041210_AddShoppingCartToDatabase.Designer.cs
+++ CharliesSnackBar/CharliesSnackBar/Data/Migrations/20180625041210_AddShoppingCartToDatabase.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CharliesSnackBar.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180625041210_AddShoppingCartToDatabase")]
+    partial class AddShoppingCartToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
