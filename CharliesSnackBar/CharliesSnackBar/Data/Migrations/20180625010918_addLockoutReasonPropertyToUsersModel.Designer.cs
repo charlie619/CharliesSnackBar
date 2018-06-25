7e7650642fb21b93fe5c76da277d7b798af21451
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-25 13:45:41 +1200

    Added Manage Users Feature (Whole)

--- CharliesSnackBar/CharliesSnackBar/Data/Migrations/20180625010918_addLockoutReasonPropertyToUsersModel.Designer.cs
+++ CharliesSnackBar/CharliesSnackBar/Data/Migrations/20180625010918_addLockoutReasonPropertyToUsersModel.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CharliesSnackBar.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180625010918_addLockoutReasonPropertyToUsersModel")]
+    partial class addLockoutReasonPropertyToUsersModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CharliesSnackBar/CharliesSnackBar/Models/ApplicationUser.cs
+++ CharliesSnackBar/CharliesSnackBar/Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -11,6 +12,9 @@ namespace CharliesSnackBar.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Display(Name ="Lockout Reason")]
+        public string LockoutReason { get; set; }
 
     }
 }
