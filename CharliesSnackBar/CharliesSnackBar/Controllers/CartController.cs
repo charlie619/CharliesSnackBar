60789ea6ffc5181ba74534a620ea5e5f1b9ac367
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-26 15:52:01 +1200

    Added Cart Controller and Index method and View

---
+++ CharliesSnackBar/CharliesSnackBar/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CharliesSnackBar.Data;
+using CharliesSnackBar.Models;
+using CharliesSnackBar.Models.OrderDetailsViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CharliesSnackBar.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET : Cart/Index
+        public IActionResult Index()
+        {
+            var detailCart = new OrderDetailsCart()
+            {
+                OrderHeader = new OrderHeader()
+            };
+
+            detailCart.OrderHeader.OrderTotal = 0;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var cart = _db.ShoppingCart.Where(x => x.ApplicationUserId == claim.Value);
+
+            if (cart != null)
+            {
+                detailCart.listCart = cart.ToList();
+            }
+
+            foreach (var list in detailCart.listCart)
+            {
+                list.MenuItem = _db.MenuItem.FirstOrDefault(x => x.Id == list.MenuItemId);
+                detailCart.OrderHeader.OrderTotal += (list.MenuItem.Price * list.Count);
+
+                if (list.MenuItem.Description.Length>100)
+                {
+                    list.MenuItem.Description = list.MenuItem.Description.Substring(0, 99) + "...";
+                }
+            }
+
+            detailCart.OrderHeader.PickUp = DateTime.Now;
+            return View(detailCart);
+        }
+    }
+}
