05e6a83affa338618e4e4c3c24ac0198a94915cb
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-07-02 01:29:05 +1200

    Added Order Confirm Get Action Method

---
+++ CharliesSnackBar/CharliesSnackBar/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CharliesSnackBar.Data;
+using CharliesSnackBar.Models.OrderDetailsViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CharliesSnackBar.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET : Confirm 
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Confirm(int id)
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var OrderDetailsViewModel = new OrderDetailsViewModel()
+            {
+                OrderHeader = _db.OrderHeader.Where(x => x.Id == id && x.UserId == claim.Value).FirstOrDefault(),
+                OrderDetails = _db.OrderDetails.Where(x => x.OrderId == id).ToList()
+            };
+
+            return View(OrderDetailsViewModel);
+        }
+    }
+}
