a7a9fb227667608702c8ca21d6be2e01e4977a64
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-07-03 00:31:18 +1200

    Created User API

---
+++ CharliesSnackBar/CharliesSnackBar/Controllers/API/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CharliesSnackBar.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CharliesSnackBar.Controllers.API
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UsersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get(string type, string query=null)
+        {
+            if (type.Equals("email")&&query!=null)
+            {
+                var customerQuery = _db.Users.Where(x => x.Email.ToLower().Contains(query.ToLower()));
+
+                return Ok(customerQuery.ToList());
+            }
+            return Ok();
+        }      
+    }
+}
