90facf365f6945db62c606911adc79f18d4a4226
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-07-05 01:03:28 +1200

    Updated EmailSending Extension Method

--- CharliesSnackBar/CharliesSnackBar/Extensions/EmailSenderExtensions.cs
+++ CharliesSnackBar/CharliesSnackBar/Extensions/EmailSenderExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using CharliesSnackBar.Services;
+using CharliesSnackBar.Utility;
 
 namespace CharliesSnackBar.Services
 {
@@ -13,6 +14,30 @@ namespace CharliesSnackBar.Services
         {
             return emailSender.SendEmailAsync(email, "Confirm your email",
                 $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
+        }
+
+        public static Task SecondOrderStatusAsync(this IEmailSender emailSender, string email, string orderNumber, string status)
+        {
+            var subject = "";
+            var message = "";
+
+            if (status==SD.StatusCancelled)
+            {
+                subject = "Order Cancelled";
+                message = "Order Number " + orderNumber + "has been Cancelled. Please contact us if you have any query";
+            }
+            if (status == SD.StatusSubmitted)
+            {
+                subject = "Order Created Successfully";
+                message = "Order Number " + orderNumber + "has been Created successfully";
+            }
+            if (status == SD.StatusReady)
+            {
+                subject = "Order Ready for Pickup";
+                message = "Order Number " + orderNumber + "is ready for Pickup! Please contact us if you have any query";
+            }
+
+            return emailSender.SendEmailAsync(email, subject, message);
         }
     }
 }
